968a45214c02d4267f2f7845119669ac08626bc5
Tanisha Kumari <tanisha_k@ph.iitr.ac.in>
2025-01-26 01:07:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Example: Smooth Scrolling for Navigation Links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute('href'));
+      target.scrollIntoView({ behavior: 'smooth' });
+    });
+  });
+  
